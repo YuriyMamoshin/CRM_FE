3b0ee9877855ba677e1e5cc741b166dfd93f30e5
Yuriy Mamoshin <yuriymamoshin@gmail.com>
2024-11-11 20:36:05 +0200

    Add relative paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import styles from "./styles.module.scss";
-
 function App() {
-    return <p className={styles["test"]}>This is gonna be our CRM</p>;
+    return <p>This is gonna be our CRM</p>;
 }
 
 export default App;
