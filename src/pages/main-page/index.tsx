dd78e3c343b9855307508eebdeadecf7f0821035
Yuriy Mamoshin <yuriymamoshin@gmail.com>
2024-11-18 16:36:17 +0200

    Styled logo, add content

--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,10 +1,23 @@
 import styles from "./styles.module.scss";
 
 import logo from "~/assets/images/logo.svg";
+
 export const MainPage: React.FC = () => {
     return (
-        <div className={styles["main-page"]}>
-            <img src={logo} alt="Logo" />
-        </div>
+        <section className={styles["main-page"]}>
+            <img src={logo} alt="Logo" className={styles["main-page__logo"]} />
+            <article
+                className={`${styles["main-page__content-wrapper"]} ${styles["content-wrapper"]}`}
+            >
+                <h1 className={styles["heading"]}>
+                    Стартуйте в IT з першим реальним проєктом від{" "}
+                    <span className={styles["heading__span"]}>TOPIZDATO!</span>{" "}
+                </h1>
+                <p className={styles["text"]}>Приєднуйтесь зараз та розвивайте свої навички</p>
+
+                <footer>Приєднатися!</footer>
+                <footer>Авторизуватися</footer>
+            </article>
+        </section>
     );
 };
